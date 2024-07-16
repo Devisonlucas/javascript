016f0498a1f03bef1be9748042dc9ce20586c9bb
Devisonlucas <devisomsilva091@gmail.com>
2024-07-16 00:50:37 -0300

    continuação

--- aula12ex/exercicio017/script.js
+++ aula12ex/exercicio017/script.js
@@ -1,1 +1,35 @@
-alert('oi')
+function tabuada() {
+var num = document.getElementById('txtn') 
+var tab = document.getElementById('seltab')   
+if (num.value.length == 0) {
+ window.alert('Por favor coloque outro numero')   
+   } else {
+   var n = Number(num.value)
+   var c = 1
+   while (c <= 10) {
+    var item = document.createElement('option')
+    item.text = `${n} x ${c} = ${n*c}`
+    tab.appendChild(item)
+    c++
+   }
+   }
+
+ }
+
+ function tabuada(){
+  var n = document.getElementById ('swdswwd')  
+  var m = document.getElementById('slkdn')
+  if(n.value.length == 0) {
+    window.alert('aldskjflad')
+  }else {
+   var n = Number(num.value) 
+   var c = 1 
+   while (c <= 10){
+    var item = document.createElement('option')
+    item.text = `${n} x ${c} = ${n*c}`
+    tab.appendChild(item)
+    c++
+   }
+
+  }
+ }
